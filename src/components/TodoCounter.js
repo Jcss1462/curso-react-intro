75e3bf367ab69b6a89744f4997e03f46cfde66d2
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-07-03 22:12:43 -0500

    Craciion del proyecto

---
+++ src/components/TodoCounter.js
@@ -0,0 +1,7 @@
+function TodoCounter() {
+    return (
+        <h1>Haz Completado 3 de 5 Todos</h1>
+    );
+}
+
+export  {TodoCounter};
---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem() {
+    return (
+        <li>
+            <span>V</span>
+            <p>Aprtender react</p>
+            <span>x</span>
+        </li>
+    );
+}
+
+export { TodoItem };

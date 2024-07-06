3792b2aeef9df43f0bcbf57adabf2f16bed68d07
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-07-05 22:39:39 -0500

    restructuracion de eliminar todo y completar todo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import logo from './platzi.webp';
 import './App.css';
-import {TodoCounter} from './components/TodoCounter';
-import {TodoSearch} from './components/TodoSearch';
-import {TodoList} from './components/TodoList';
-import {TodoItem} from './components/TodoItem';
+import { TodoCounter } from './components/TodoCounter';
+import { TodoSearch } from './components/TodoSearch';
+import { TodoList } from './components/TodoList';
+import { TodoItem } from './components/TodoItem';
 import { CreateTodoButton } from './components/CreateTodoButton';
 import React from 'react';
 
 function App() {
 
-  const [searchValue, setSearchValue]=React.useState("");
-  const [toDos, setTodos]=React.useState([
-    {text: "cortar cebolla", completed:true},
-    {text: "Hacer curso de react", completed:false}
+  const [searchValue, setSearchValue] = React.useState("");
+  const [toDos, setTodos] = React.useState([
+    { text: "cortar cebolla", completed: true },
+    { text: "Hacer curso de react", completed: false }
   ]);
 
-  const completedTodos= toDos.filter((prev)=>prev.completed==true).length;
-  const totalTodos= toDos.length;
+  const completedTodos = toDos.filter((prev) => prev.completed == true).length;
+  const totalTodos = toDos.length;
 
-  console.log(completedTodos);
+  const competeTodo = (index) => {
+    const newTodo = [...toDos];
+    newTodo[index].completed = !newTodo[index].completed;
+    setTodos(newTodo)
+  }
+
+  const deletTodo = (index) => {
+    setTodos(toDos.filter((elem, idx) => {
+      return index == idx ? "" : elem;
+    }))
+  }
 
 
   return (
     <div id="app">
-      <TodoCounter  completed = {completedTodos} total={totalTodos}></TodoCounter>
+      <TodoCounter completed={completedTodos} total={totalTodos}></TodoCounter>
       <TodoSearch
         searchValue={searchValue}
         setSearchValue={setSearchValue}
@@ -31,17 +41,17 @@ function App() {
 
       <TodoList>
         {toDos.map((todo, index) =>
-          todo.text.toLowerCase().includes(searchValue.toLowerCase())?(<TodoItem key={todo.text} text={todo.text} completed={todo.completed} todos ={toDos} setTodos={setTodos} index={index}></TodoItem>):""
-          )}
+          todo.text.toLowerCase().includes(searchValue.toLowerCase()) ? (<TodoItem key={todo.text} text={todo.text} completed={todo.completed} onComplete={() => competeTodo(index)} onDelete={() => deletTodo(index)}></TodoItem>) : ""
+        )}
       </TodoList>
 
       <CreateTodoButton></CreateTodoButton>
-      
-    </div> 
+
+    </div>
   );
 }
 
 
 
-export  {App};
+export { App };
 
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,26 +1,12 @@
-function TodoItem({ text, completed, todos, setTodos, index }) {
+function TodoItem({ text, completed, onComplete ,onDelete }) {
 
-    function completarTodo(){
-        setTodos(todos.map((prev, idx) => {
-            if (idx == index) {
-                prev.completed = !prev.completed;
-            }
-            return prev;
-        }))
-    }
-
-    function eliminarTodo(){
-        setTodos(todos.filter((elem,idx) => {
-            return index==idx?"":elem;
-        }))
-    }
 
 
     return (
         <li>
-            <span onClick={completarTodo}>V</span>
+            <span onClick={onComplete}>V</span>
             <p className={`${completed && "--complete"}`}>{text}</p>
-            <span onClick={eliminarTodo}>x</span>
+            <span onClick={onDelete}>x</span>
         </li>
     );
 }

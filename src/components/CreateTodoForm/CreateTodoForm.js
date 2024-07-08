e20e17180c03ce999f3e77011da42369856cd1e4
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-07-08 17:26:50 -0500

    curepo del formulario de guardado

--- src/components/CreateTodoForm/CreateTodoForm.js
+++ src/components/CreateTodoForm/CreateTodoForm.js
@@ -4,14 +4,32 @@ import { TodoContext } from "../../shared/contexts/TodoContext";
 
 function CreateTodoForm() {
 
-    const { setOpenModal } = React.useContext(TodoContext);
+    const { setOpenModal} = React.useContext(TodoContext);
+
+    function saveTodo(event){
+        event.preventDefault();
+        //saveItem({ text: "cortar cebolla", completed: true });
+    }
 
     return (
-        <div id="myModal" class="modal">
-            <div class="modal-content">
-                <button class="close-btn" id="closeModalBtn" onClick={()=>{setOpenModal(false)}}>&times;</button>
-                <h2>Modal Title</h2>
-                <p>Some information goes here...</p>
+        <div id="myModal" className="modal">
+            <div className="modal-content">
+                <button className="close-btn" id="closeModalBtn" onClick={() => { setOpenModal(false) }}>&times;</button>
+                <div id="formContainer">
+                    <form onSubmit={(event)=>{
+                        saveTodo(event);
+
+                    }}>
+                        <label><b>Escribe tu nuevo TODO</b></label>
+                     
+                        <textarea
+                            placeholder="Escribe nuevo Todo" />
+
+                        <button  trype="submit" className="guardarTodoButton">
+                            <b>Guardar</b>
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     );
